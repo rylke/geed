963e421f2d14554bb8b7c24e329b12884691027a
elyor <elyor.kasimov@gmail.com>
2017-05-08 18:42:20 +0200

    Initial Commit

---
+++ geed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  geed
+//
+//  Created by Elyor on 08/05/2017.
+//  Copyright © 2017 Elyor Kasimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
